b3e829eaebba4f7f40a8922ec8d47c52126f2272
PM <12273891+n33pm@users.noreply.github.com>
2021-05-20 14:44:27 +0200

    feat(): example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,38 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import "react-app-polyfill/ie11"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import VisualWindow, {VisualWindowChildProps} from "../src"
+
+interface IData {
+    odd: boolean
+}
+
+const data = new Array(1000).fill(true).map(
+    (_, x) =>
+        ({
+            odd: x % 2 !== 0,
+        } as IData),
+)
+
+const Row = React.forwardRef<HTMLDivElement, VisualWindowChildProps>((props, ref) => {
+    const {data, index, style} = props
+    return (
+        <div ref={ref} style={style}>
+            <div>
+                Row {index} - odd: {(data as IData[])[index].odd ? `true` : `false`}
+            </div>
+        </div>
+    )
+})
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
-};
+    return (
+        <div>
+            <VisualWindow defaultItemHeight={18} itemData={data} className="tableClass">
+                {Row}
+            </VisualWindow>
+        </div>
+    )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
