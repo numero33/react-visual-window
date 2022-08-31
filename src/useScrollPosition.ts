4bb7406c9edb29d547a0158d3688a2248d19b94d
PM <12273891+n33pm@users.noreply.github.com>
2022-08-31 12:56:02 +0200

    feat(): add container view

--- src/useScrollPosition.ts
+++ src/useScrollPosition.ts
@@ -1,26 +1,52 @@
-import {useEffect, useState} from 'react'
+import {MutableRefObject, useLayoutEffect, useEffect, useState} from 'react'
 
-const getPosition = (isClient: boolean) => {
-    return {
-        scrollY: isClient ? window.scrollY : undefined,
-    }
+type ElementRef = MutableRefObject<HTMLElement | null>
+const isBrowser = typeof window !== `undefined`
+const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
+
+const getScrollPosition = ({element}: {element?: HTMLElement}) => {
+    if (!isBrowser) return {x: 0, y: 0}
+    if (element) return {x: element.scrollLeft, y: element.scrollTop}
+    return {x: window.scrollX, y: window.scrollY}
 }
 
-export default function useScrollPosition() {
-    const isClient = typeof window === 'object'
+export interface useScrollPositionProps {
+    element?: ElementRef
+    wait?: number
+}
 
-    const [scrollPosition, setScrollPosition] = useState(getPosition(isClient))
+export interface useScrollPositionReturn {
+    x: number
+    y: number
+}
 
-    useEffect(() => {
-        if (!isClient) return undefined
+export default function useScrollPosition({element, wait}: useScrollPositionProps): useScrollPositionReturn {
+    const useElement = element?.current ?? undefined
+    const [position, setPosition] = useState({} as useScrollPositionReturn)
 
-        function handleScroll() {
-            setScrollPosition(getPosition(isClient))
+    let throttleTimeout: number | null = null
+
+    const callBack = () => {
+        setPosition(getScrollPosition({element: useElement}))
+        throttleTimeout = null
+    }
+
+    useIsomorphicLayoutEffect(() => {
+        if (!isBrowser) return undefined
+
+        const handleScroll = () => {
+            if (throttleTimeout === null && wait) throttleTimeout = window.setTimeout(callBack, wait)
+            else callBack()
         }
 
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, [isClient])
+        const container = useElement ?? window
+        container?.addEventListener('scroll', handleScroll, {passive: true})
 
-    return scrollPosition
+        return () => {
+            container?.removeEventListener('scroll', handleScroll)
+            if (throttleTimeout) clearTimeout(throttleTimeout)
+        }
+    }, [useElement])
+
+    return position
 }
